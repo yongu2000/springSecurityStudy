641716e80879e8d1da38024e09d8846fdc4a1469
Yong U Cho <yongu2000@naver.com>
2025-02-13 18:14:10 +0900

    commit test

--- securitybasic/src/main/java/com/example/securitybasic/SecurityBasicApplication.java
+++ securitybasic/src/main/java/com/example/securitybasic/SecurityBasicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuritybasicApplication {
+public class SecurityBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuritybasicApplication.class, args);
+		SpringApplication.run(SecurityBasicApplication.class, args);
 	}
 
 }
