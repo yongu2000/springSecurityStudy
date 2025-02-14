3be283bf3a234dc1536abafb9038fb94733da383
Yong U Cho <yongu2000@naver.com>
2025-02-14 14:08:35 +0900

    jwt init commit

---
+++ springsecurityjwt/src/main/java/com/example/springsecurityjwt/SpringsecurityjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityjwtApplication.class, args);
+	}
+
+}
